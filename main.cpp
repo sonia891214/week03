8290619ae6cf64684b7b9da956530347774c9f24
sonia891214 <80247051+sonia891214@users.noreply.github.com>
2021-03-08 16:27:00 +0800

    Add files via upload
    week03上課內容

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include<GL/glut.h>
+void display()
+{
+    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+    glutSolidTeapot(0.3);
+    glutSwapBuffers();
+}
+int main(int argc,char**argv)
+{
+    glutInit(&argc,argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_DEPTH);
+    glutCreateWindow("08160103 我是大美女");
+    glutDisplayFunc(display);
+    glutMainLoop();
+}
